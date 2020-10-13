9f8d1e9d2a550bf1dcc1b5f19cbdc6ce89a3ed48
Idris Khan <ricepoo123@gmail.com>
2020-10-13 19:50:49 +0100

    Inital Starter files

---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.use(
+  cors({
+    origin: 'http://127.0.0.1:3000',
+    credentials: true,
+  })
+);
+// SET Security HTTP headers
+// app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'", 'https:', 'http:', 'data:', 'ws:'],
+      baseUri: ["'self'"],
+      fontSrc: ["'self'", 'https:', 'http:', 'data:'],
+      imgSrc: ["'self'", 'data'],
+      scriptSrc: ["'self'", 'https:', 'http:', 'blob:'],
+      styleSrc: ["'self'", "'unsafe-inline'", 'https:', 'http:'],
+    },
+  })
+);
+
+// dev logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Limit number of requests from the same IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+
+app.use('/api', limiter);
+
+// body parser, reading data from body into req.body
+// middleware that sits in the middle of req(uests) and res(ponse) - we do this to get access to the body of a HTTP request
+app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
+
+// Data sanitization against NoSQL query injections
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Protect against request parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize, ',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+// ROUTES
+
+// UNHANDLED ROUTE ERROR MIDDLEWARE
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 400));
+});
+
+// GLOBAL ERROR HANDLER MIDDLEWARE
+app.use(globalErrorHandler);
+
+module.exports = app;
