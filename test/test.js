5d8536b04afdc2ddaabe1285577f0f56f0321821
Idris Khan <idris.khan303@gmail.com>
2020-10-26 19:21:59 +0000

    More on chai testing

--- test/test.js
+++ test/test.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const chai = require('chai');
 const should = require('chai').should();
 const chaiHttp = require('chai-http');
-// const { expect } = require('chai');
+const { expect } = require('chai');
+const chaiLike = require('chai-like');
+const chaiThings = require('chai-things');
 
 const server = require('../server');
 const Card = require('../models/cardModel');
@@ -28,6 +30,8 @@ const Card = require('../models/cardModel');
 // });
 
 chai.use(chaiHttp);
+chai.use(chaiLike);
+chai.use(chaiThings);
 
 describe('Cards', () => {
   // beforeEach((done) => {
@@ -36,13 +40,28 @@ describe('Cards', () => {
   // });
 
   describe('/GET card', () => {
-    it('It should GET all the books', (done) => {
+    it('It should GET all the cards', (done) => {
       chai
         .request(server)
         .get('/api/v0/cards')
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('object');
+          res.body.should.have.property('status');
+          res.body.should.have.property('results');
+          res.body.should.have.property('data');
+          res.body.should.have.nested.property('data.card');
+
+          const card = res.body.data.card;
+          card.should.be.a('array');
+
+          card.forEach((item) => {
+            item.should.have.property('question');
+            item.should.have.property('answer');
+            item.should.have.property('user');
+            item.should.have.property('_id');
+          });
+
           done();
         });
     });
