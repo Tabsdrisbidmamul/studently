7407a37df620bae4ac895a652293b1073be755a4
Idris Khan <idris.khan303@gmail.com>
2020-11-06 18:06:28 +0000

    Added CORS to the API

--- app.js
+++ app.js
@@ -19,12 +19,8 @@ const AppError = require('./utils/appError');
 
 const app = express();
 
-app.use(
-  cors({
-    origin: 'http://127.0.0.1:3000',
-    credentials: true,
-  })
-);
+// implement cors
+app.use(cors());
 
 // SET Security HTTP headers
 // app.use(helmet());
